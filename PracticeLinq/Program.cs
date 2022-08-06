7bcd2d0c889e80d2c6a819c8a4dde3589d160cd2
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-06 15:45:21 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() {"Stalker", "The Witcher", "GTA", "Mario" };
+
+            var videogm = games.Max();
+            var sorted = games.OrderBy(x => x);
+            games.Sort();
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
